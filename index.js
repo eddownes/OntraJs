e83f4aefe66b54ca6ceafd5ef1fdeb9401142d37
Daniel Ashcraft <Dashcraft@Megans-MacBook-Pro.local>
2018-05-25 15:33:20 -0500

    Get started creating ontraport api interface

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-export default class OntraportService {}
+import fetch from 'node-fetch';
+
+export default class OntraportService {
+  constructor(config) {
+    this.config = config;
+
+    this.headers = {
+      'Content-Type': 'application/json',
+      'Api-key': this.config.apiKey,
+      'Api-Appid': this.config.appId
+    };
+  }
+}
