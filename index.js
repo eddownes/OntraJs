bc6bd053b7fd466c04ead6f8a2bca3b14829d0ca
Daniel Ashcraft <Daniel.Ashcraft@ofashandfire.com>
2018-09-07 17:17:08 -0500

    Update the stuffs

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ export default class OntraportService {
 
     this.headers = {
       'Content-Type': 'application/json',
-      'Api-key': this.config.apiKey,
-      'Api-Appid': this.config.appId
+      'Api-key': this.config.API_KEY,
+      'Api-Appid': this.config.APP_ID
     };
+    this.base_api_url = 'https://api.ontraport.com/';
   }
+
+  getContactById() {}
+
+  getContactsByIds() {}
+
+  deleteContact() {}
+
+  postContacts() {}
+
+  upsertContacts() {}
+
+  getCampaigns() {}
+
+  postCampaigns() {}
+
+  putCampaigns() {}
+
+  deleteCampaigns() {}
 }
