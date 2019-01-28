8298ec898770e25ceec9cebf8e8e8b57b9e9c206
dashcraft <ashcraftbookkeeping@gmail.com>
2019-01-28 11:47:26 -0600

    Updated ontrajs file

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-const { GraphQLServer } = require('graphql-yoga')
-const fetch = require('node-fetch')
-const baseURL = `https://rest-demo-hyxkwbnhaz.now.sh`;
-const resolvers = {
+const files = require('./lib/files');
+const inquirer  = require('./lib/inquirer');
+const figlet = require('figlet');
+const clear = require('clear');
+const chalk = require('chalk');
+const fs = require('fs');
+const CLI         = require('clui')
+const Spinner     = CLI.Spinner;
 
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ontra-Js', { horizontalLayout: 'full' })
+  )
+);
+
+if (files.directoryExists('./server')) {
+  console.log(chalk.red('Server has already been created, run update instead!'));
+  process.exit();
 }
 
-const server = new GraphQLServer({
-  typeDefs: './ontraport_schema.graphql',
-  resolvers,
-})
+const run = async () => {
+  const credentials = await inquirer.askGithubCredentials();
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  let str = [`API_KEY = "${credentials.api_key}"`, `APP_ID = "${credentials.app_id}"`].join('\n')
+  const status = new Spinner('Creating Env File...');
+  status.start();
+  fs.writeFile('.env', str, res => {
+    status.stop();
+    console.log(chalk.green('Successfully saved Ontraport Credentials to Env file!'));
+  })
+  
+}
+
+run();
