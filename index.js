1706896dd806ed58b54d9764c9ea2a893b1f3d27
dashcraft <ashcraftbookkeeping@gmail.com>
2019-01-29 12:13:56 -0600

    Updated to create a schema generator function.

--- index.js
+++ index.js
@@ -27,56 +27,34 @@ if (files.directoryExists('./server')) {
   process.exit();
 }
 const run = async () => {
-  const questions = [
-    {
-      name: 'app_id',
-      type: 'input',
-      message: 'Enter your Ontraport APP ID:',
-      validate: function( value ) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter your Ontraport APP ID.';
-        }
-      }
-    },
-    {
-      name: 'api_key',
-      type: 'password',
-      message: 'Enter your Ontraport API Key:',
-      validate: function(value) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter your Ontraport API Key';
-        }
-      }
-    }
-  ];
-  let credentials = await inquirer.askOntraportCredentials(questions);
+  let credentials = await inquirer.askOntraportCredentials();
   let str = [`API_KEY = "${credentials.api_key}"`, `APP_ID = "${credentials.app_id}"`].join('\n')
-  const status = new Spinner('Scaffolding files...');
-  status.start();
-  fs.writeFile('.env', str, res => {
+  
+  if(!files.directoryExists(`${credentials.appname}`)){
+    fs.mkdir(`./${credentials.appname}`);
+  } else {
+    console.log(chalk.red('App Directory Already Exists!'));
+    process.exit();
+  }
+  
+  let appRoot = `./${credentials.appname}`;
+
+  fs.writeFile(`${appRoot}/.env`, str, res => {
     console.log(chalk.green('Successfully saved Ontraport Credentials to Env file!'));
   })
   
   if(!files.directoryExists('./services')){
-    fs.mkdir('./services');
+    fs.mkdir(`${appRoot}/services`);
   }
 
+  const status = new Spinner('Scaffolding files...');
+  status.start();
   let createOs = execute('npm root -g', function(location){
-    fs.createReadStream(`${location.replace("\\","/").replace("\n", "")}/ontrajs/lib/ontraport_service.txt`).pipe(fs.createWriteStream(`./services/ontraport_service.js`))
+    fs.createReadStream(`${location.replace("\\","/").replace("\n", "")}/ontrajs/lib/ontraport_service.txt`).pipe(fs.createWriteStream(`${appRoot}/services/ontraport_service.js`))
   })
-
-  let stream = fs.createWriteStream('schema.graphql');
-  stream.once('open', function(fd) {
-    stream.write("type User {\n");
-    stream.write("\tid : String!\n");
-    stream.write('}')
-    stream.end();
-  });
   status.stop();
+  
+  // files.createTypeFile( `${appRoot}`, 'schema', contents);
 }
 
 run()
